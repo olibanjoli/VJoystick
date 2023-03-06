e9984f9f857ad9a2fe3e3586586e4910126702ab
Oliver Zürcher <oliver.zuercher@zuehlke.com>
2023-03-06 22:33:21 +0100

    testing docusaurus

--- VbarJoystick/Program.cs
+++ VbarJoystick/Program.cs
@@ -29,6 +29,66 @@ long maxRZ = 0;
 long maxSL0 = 0;
 long maxSL1 = 0;
 
+var pitch = 0;
+var tail = 0;
+var ail = 0;
+var elev = 0;
+
+int switches = 0;
+var motorOff = false;
+var motorOn = false;
+var motorIdle = false;
+
+var bank1 = false;
+var bank2 = false;
+var bank3 = false;
+
+var buddy = false;
+var master = false;
+
+var option1A = false;
+var option1B = false;
+var option1Middle = false;
+
+var option2A = false;
+var option2B = false;
+var option2Middle = false;
+
+var option3A = false;
+var option3B = false;
+var option3Middle = !option3A && !option3A;
+        
+var option4A = false;
+var option4B = false;
+var option4Middle = !option4A && !option4A;
+
+var sendData = new byte[10];
+byte[] receiveBytes;
+
+string GetBitsAsString(int number)
+{
+    const int IntSize = sizeof(int) * 8; // Number of bits in an int
+
+    // Create a char array to hold the bits, initialize to '0'
+    char[] bits = new char[IntSize];
+    for (int i = 0; i < IntSize; i++)
+    {
+        bits[i] = '0';
+    }
+
+    // Convert the number to binary and fill in the char array
+    int index = IntSize - 1;
+    while (number > 0 && index >= 0)
+    {
+        bits[index] = (number & 1) == 1 ? '1' : '0';
+        number >>= 1;
+        index--;
+    }
+
+    // Convert the char array to a string and return
+    return new string(bits);
+}
+
 AnsiConsole.Status()
     .AutoRefresh(true)
     .Spinner(Spinner.Known.Default)
@@ -86,21 +146,31 @@ AnsiConsole.Status()
         uint dllVer = 0, drvVer = 0;
         var match = joystick.DriverMatch(ref dllVer, ref drvVer);
 
-        if (match)
-            AnsiConsole.MarkupLine("[grey]Version of Driver Matches DLL Version ({0:X})[/]\n", dllVer);
-        else
-            AnsiConsole.MarkupLine("[grey]Version of Driver ({0:X}) does NOT match DLL Version ({1:X})[/]\n", drvVer,
-                dllVer);
+        // if (match)
+        //     AnsiConsole.MarkupLine("[grey]Version of Driver Matches DLL Version ({0:X})[/]\n", dllVer);
+        // else
+        //     AnsiConsole.MarkupLine("[grey]Version of Driver ({0:X}) does NOT match DLL Version ({1:X})[/]\n", drvVer,
+        //         dllVer);
 
         AnsiConsole.MarkupLine("[grey]starting udp client[/]");
 
         udpClient = new UdpClient(1025);
+        
+        //AnsiConsole.MarkupLine("waiting for connection from TX...");
+
+        AnsiConsole.MarkupLine("[green3_1]Ready, please start [dodgerblue1]IP UDP Simulator[/] App on VBar Transmitter[/]");
+        ctx.Status("waiting for connection from VBar TX...");
+        
+        var remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 1025);
+
+        receiveBytes = udpClient.Receive(ref remoteIpEndPoint);
+        
+        var rule = new Rule($"Connected to TX [blue]Oli[/] [blue dim]1023023[/] (10.0.0.201)");
+        AnsiConsole.Write(rule);
     });
 
-AnsiConsole.Clear();
 
-var rule = new Rule($"Connected to TX [blue]Oli[/] [blue dim]1023023[/] (10.0.0.201)");
-AnsiConsole.Write(rule);
+AnsiConsole.WriteLine();
 
 var table = new Table()
     .Border(TableBorder.Rounded)
@@ -109,7 +179,7 @@ var table = new Table()
     .AddRow("Pitch", "0")
     .AddRow("Tail", "0")
     .AddRow("Elevator", "0")
-    .AddRow("Nick", "0")
+    .AddRow("Aileron", "0")
     .AddRow("Motor")
     .AddRow("Bank")
     .AddRow("Option 1")
@@ -117,7 +187,6 @@ var table = new Table()
     .AddRow("Option 3")
     .AddRow("Option 4")
     .AddRow("Buddy");
-
 
 AnsiConsole.Live(table)
     .AutoClear(false)
@@ -127,45 +196,11 @@ AnsiConsole.Live(table)
     {
         var remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 1025);
 
-        var sendData = new byte[10];
-
-        var pitch = 0;
-        var tail = 0;
-        var ail = 0;
-        var elev = 0;
-
-        var motorOff = false;
-        var motorOn = false;
-        var motorIdle = false;
-
-        var bank1 = false;
-        var bank2 = false;
-        var bank3 = false;
-
-        var buddy = false;
-        var master = false;
-
-        var option1A = false;
-        var option1B = false;
-        var option1Middle = false;
-
-        var option2A = false;
-        var option2B = false;
-        var option2Middle = false;
-
-        var option3A = false;
-        var option3B = false;
-        var option3Middle = !option3A && !option3A;
-        
-        var option4A = false;
-        var option4B = false;
-        var option4Middle = !option4A && !option4A;
-
         while (true)
         {
             try
             {
-                var receiveBytes = udpClient.Receive(ref remoteIpEndPoint);
+                receiveBytes = udpClient.Receive(ref remoteIpEndPoint);
 
                 var id = receiveBytes[0] & 0xFF | (receiveBytes[1] & 0xFF) << 8;
                 var version = receiveBytes[2] & 0xFF;
@@ -175,11 +210,11 @@ AnsiConsole.Live(table)
                 {
                     continue;
                 }
-
+                
                 switch (command)
                 {
                     case 1: // plain control data
-                        var switches = receiveBytes[4] & 0xFF | (receiveBytes[5] & 0xFF) << 8 |
+                        switches = receiveBytes[4] & 0xFF | (receiveBytes[5] & 0xFF) << 8 |
                                        (receiveBytes[6] & 0xFF) << 16
                                        | (receiveBytes[7] & 0xFF) << 24;
                         //Console.WriteLine(Convert.ToString(switches, 2));
@@ -189,8 +224,8 @@ AnsiConsole.Live(table)
                         motorIdle = !motorOff && !motorOn;
 
                         bank1 = (switches & 0b_0000_0000_0000_0000_0000_0000_0000_0100) != 0;
-                        bank2 = (switches & 0b_0000_0000_0000_0000_0000_0000_0000_1000) != 0;
-                        bank3 = !bank1 && !bank2;
+                        bank3 = (switches & 0b_0000_0000_0000_0000_0000_0000_0000_1000) != 0;
+                        bank2 = !bank1 && !bank3;
 
                         buddy = (switches & 0b_0000_0000_0000_0000_0000_0000_0001_0000) != 0;
                         master = !buddy;
@@ -314,9 +349,9 @@ AnsiConsole.Live(table)
                             .UpdateCell(0, 1, pitch.ToString())
                             .UpdateCell(1, 1, tail.ToString())
                             .UpdateCell(2, 1, elev.ToString())
-                            .UpdateCell(3, 1, tail.ToString())
+                            .UpdateCell(3, 1, ail.ToString())
                             .UpdateCell(4, 1, motorOff ? "off" : motorIdle ? "Idle" : "On")
-                            .UpdateCell(5, 1, bank1 ? "1" : bank2 ? "2" : bank3 ? "3" : "AR")
+                            .UpdateCell(5, 1, bank1 ? "1" : bank2 ? "2" : "3" )
                             .UpdateCell(6, 1, option1A ? "A" : option1B ? "B" : "Middle")
                             .UpdateCell(7, 1, option2A ? "A" : option2B ? "B" : "Middle")
                             .UpdateCell(8, 1, option3A ? "A" : option3B ? "B" : "Middle")
